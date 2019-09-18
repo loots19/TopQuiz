f04dce9de2b34a5c0e8c53f1fb08daf224822aec
loots <ludotracks@hotmail.com>
2019-09-18 09:06:19 +0200

    exercice

--- app/src/main/java/com/e/topquiz/controller/GameActivity.java
+++ app/src/main/java/com/e/topquiz/controller/GameActivity.java
@@ -1,9 +1,14 @@
 package com.e.topquiz.controller;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
@@ -26,7 +31,12 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     private Question mCurrentQuestion;
 
     private int mScore;
-    private int mNumberOfQuestion;
+    private int mNumberOfQuestions;
+
+    public static final String BUNDLE_EXTRA_SCORE = "BUNDLE_EXTRA_SCORE";
+    public static final String BUNDLE_STATE_SCORE = "currentScore";
+    public static final String BUNDLE_STATE_QUESTION = "currentQuestion";
+    private boolean mEnableTouchEvent;
 
 
     @Override
@@ -34,10 +44,20 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
+        System.out.println("GameActivity::onCreate()");
+
+
         mQuestionBank = this.generateQuestions();
 
-        mScore = 0;
-        mNumberOfQuestion = 5;
+
+        if (savedInstanceState != null) {
+            mScore = savedInstanceState.getInt(BUNDLE_STATE_SCORE);
+            mNumberOfQuestions = savedInstanceState.getInt(BUNDLE_STATE_QUESTION);
+        } else {
+            mScore = 0;
+            mNumberOfQuestions = 4;
+        }
+        mEnableTouchEvent = true;
 
 
         mQuestionTextView = (TextView) findViewById(R.id.activity_game_question_text);
@@ -61,6 +81,14 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
     }
 
     @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        outState.putInt(BUNDLE_STATE_SCORE, mScore);
+        outState.putInt(BUNDLE_STATE_QUESTION, mNumberOfQuestions);
+
+        super.onSaveInstanceState(outState);
+    }
+
+    @Override
     public void onClick(View v) {
         int responseIndex = (int) v.getTag();
 
@@ -73,74 +101,129 @@ public class GameActivity extends AppCompatActivity implements View.OnClickListe
             // wrong answer
             Toast.makeText(this, "wrong answer!", Toast.LENGTH_SHORT).show();
         }
-        if (--mNumberOfQuestion == 0) {
-        // end of game
-            endGame();
-    }else{
-            mCurrentQuestion = mQuestionBank.getQuestion();
-            displayQuestion(mCurrentQuestion);
-        }
+        mEnableTouchEvent = false;
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                mEnableTouchEvent = true;
+                if (--mNumberOfQuestions == 0) {
+                    // end of game
+                    endGame();
+                } else {
+                    mCurrentQuestion = mQuestionBank.getQuestion();
+                    displayQuestion(mCurrentQuestion);
+                }
+            }
+        },2000);
+
+    }
+
+    @Override
+    public boolean dispatchTouchEvent(MotionEvent ev) {
+        return mEnableTouchEvent && super.dispatchTouchEvent(ev);
     }
 
     private void endGame() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("Well done!")
+                .setMessage("Your score is " + mScore)
+                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Intent intent = new Intent();
+                        intent.putExtra(BUNDLE_EXTRA_SCORE, mScore);
+                        setResult(RESULT_OK, intent);
+                        finish();
+                    }
+                })
+                .create()
+                .show();
     }
 
-    private void displayQuestion ( final Question question){
-            mQuestionTextView.setText(question.getQuestion());
-            mAnswerButton1.setText(question.getChoiceList().get(0));
-            mAnswerButton2.setText(question.getChoiceList().get(1));
-            mAnswerButton3.setText(question.getChoiceList().get(2));
-            mAnswerButton4.setText(question.getChoiceList().get(3));
-
-
-        }
-
-
-        private QuestionBank generateQuestions () {
-
-            Question question1 = new Question("Qui est le meilleur ami de Elsa?",
-                    Arrays.asList("Olaf", "Dingo", "Snoopy", "Hans"),
-                    0);
-
-            Question question2 = new Question("ou vie la reine des neige?",
-                    Arrays.asList("New York", "Paris", "Arendal", "Rome"),
-                    2);
-
-            Question question3 = new Question(" Quel animal est Peppa Pig?",
-                    Arrays.asList("Vache", "Zebre", "Cochon", "Poule"),
-                    2);
-
-            Question question4 = new Question("ou habite le pere noel?",
-                    Arrays.asList("Pole nord", "Inde", "Bouffemont", "Paris"),
-                    0);
-
-            Question question5 = new Question("Que mange le mouton?",
-                    Arrays.asList("Mc do", "Pizza", "Herbe", "Viande"),
-                    2);
-
-            Question question6 = new Question("Que mange la baleine?",
-                    Arrays.asList("du Foint", "du Plancton", "des Oeufs", "des Moutons"),
-                    1);
-
-            Question question7 = new Question("Ou habite la pricesse Sophia?",
-                    Arrays.asList("Maroc", "Paris", "Istamboul", "Avalor"),
-                    3);
-
-            Question question8 = new Question("Le traineau du Pere noel est trainé par?",
-                    Arrays.asList("Chevaux", "Lions", "Dindes", "Rennes"),
-                    3);
-
-            Question question9 = new Question("2+2 est egale a?",
-                    Arrays.asList("0", "2", "4", "6"),
-                    2);
-
-            Question question10 = new Question("Les playmobiles sont fabriqués en?",
-                    Arrays.asList("Carton", "Plastique", "Terre", "Bois"),
-                    1);
-
-            return new QuestionBank(Arrays.asList(question1, question2, question3, question4, question5, question6, question7, question8, question9, question10));
-        }
+    private void displayQuestion(final Question question) {
+        mQuestionTextView.setText(question.getQuestion());
+        mAnswerButton1.setText(question.getChoiceList().get(0));
+        mAnswerButton2.setText(question.getChoiceList().get(1));
+        mAnswerButton3.setText(question.getChoiceList().get(2));
+        mAnswerButton4.setText(question.getChoiceList().get(3));
 
 
     }
+
+
+    private QuestionBank generateQuestions() {
+
+        Question question1 = new Question("Qui est le meilleur ami de Elsa?",
+                Arrays.asList("Olaf", "Dingo", "Snoopy", "Hans"),
+                0);
+
+        Question question2 = new Question("ou vie la reine des neige?",
+                Arrays.asList("New York", "Paris", "Arendal", "Rome"),
+                2);
+
+        Question question3 = new Question(" Quel animal est Peppa Pig?",
+                Arrays.asList("Vache", "Zebre", "Cochon", "Poule"),
+                2);
+
+        Question question4 = new Question("ou habite le pere noel?",
+                Arrays.asList("Pole nord", "Inde", "Bouffemont", "Paris"),
+                0);
+
+        Question question5 = new Question("Que mange le mouton?",
+                Arrays.asList("Mc do", "Pizza", "Herbe", "Viande"),
+                2);
+
+        Question question6 = new Question("Que mange la baleine?",
+                Arrays.asList("du Foint", "du Plancton", "des Oeufs", "des Moutons"),
+                1);
+
+        Question question7 = new Question("Ou habite la pricesse Sophia?",
+                Arrays.asList("Maroc", "Paris", "Istamboul", "Avalor"),
+                3);
+
+        Question question8 = new Question("Le traineau du Pere noel est trainé par?",
+                Arrays.asList("Chevaux", "Lions", "Dindes", "Rennes"),
+                3);
+
+        Question question9 = new Question("2+2 est egale a?",
+                Arrays.asList("0", "2", "4", "6"),
+                2);
+
+        Question question10 = new Question("Les playmobiles sont fabriqués en?",
+                Arrays.asList("Carton", "Plastique", "Terre", "Bois"),
+                1);
+
+        return new QuestionBank(Arrays.asList(question1, question2, question3, question4, question5, question6, question7, question8, question9, question10));
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        System.out.println("GameActivity::onResume()");
+    }
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("GameActivity::onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("GameActivity::onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("GameActivity::onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("GameActivity::onPause()");
+    }
+}
--- app/src/main/java/com/e/topquiz/controller/MainActivity.java
+++ app/src/main/java/com/e/topquiz/controller/MainActivity.java
@@ -1,8 +1,10 @@
 package com.e.topquiz.controller;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
@@ -20,19 +22,40 @@ public class MainActivity extends AppCompatActivity {
     private TextView mGreetinText;
     private EditText mNameInput;
     private Button mPlayButton;
+    private Button mScoreButton;
     private User mUser;
+    public static final int Game_Activity_Request_Code = 1;
+    private SharedPreferences mPreferences;
 
+    public static final String PREF_KEY_SCORE = "PREF_KEY_SCORE";
+    public static final String PREF_KEY_FIRSTNAME = "PREF_KEY_FIRSTNAME";
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (Game_Activity_Request_Code == requestCode && RESULT_OK == resultCode) {
+            int score = data.getIntExtra(GameActivity.BUNDLE_EXTRA_SCORE,0);
+            mPreferences.edit().putInt(PREF_KEY_SCORE,score).apply();
+        }
+    }
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        System.out.println("MainActivity :: onStart()");
 
         mUser = new User();
+
+        mPreferences = getPreferences(MODE_PRIVATE);
         mGreetinText = (TextView) findViewById(R.id.activity_main_greetin_txt);
         mNameInput = (EditText) findViewById(R.id.activity_main_name_input);
         mPlayButton = (Button) findViewById(R.id.activity_main_btn);
+        mScoreButton = (Button)findViewById(R.id.activity_score_btn);
+
 
         mPlayButton.setEnabled(false);
+
+        greetUser();
 
         mNameInput.addTextChangedListener(new TextWatcher() {
             @Override
@@ -59,13 +82,62 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(View v) {
                     String firstnanme = mNameInput.getText().toString();
                     mUser.setFirstName(firstnanme);
-                    Intent gameActivity = new Intent(MainActivity.this, GameActivity.class);
-                    startActivity(gameActivity);
+
+                    mPreferences.edit().putString(PREF_KEY_FIRSTNAME,mUser.getFirstName()).apply();
+                    Intent gameActivityIntent = new Intent(MainActivity.this, GameActivity.class);
+                    startActivityForResult(gameActivityIntent,Game_Activity_Request_Code);
 
                 }
 
             });
+
         }
 
+
+    }
+
+    private void greetUser() {
+        String Firstname = mPreferences.getString(PREF_KEY_FIRSTNAME,null);
+
+        if (null!= Firstname){
+            int score = mPreferences.getInt(PREF_KEY_SCORE,0);
+            String fullText = "welcomme back," + Firstname
+            + "!\n last time your score was " + score
+            + ", do better this time";
+            mGreetinText.setText(fullText);
+            mNameInput.setText(Firstname);
+            mNameInput.setSelection(Firstname.length());
+            mPlayButton.setEnabled(true);
+        }
+    }
+
+    @Override
+    protected void onPostResume() {
+        super.onPostResume();
+        System.out.println("GameActivity::onResume()");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        System.out.println("GameActivity::onStart()");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        System.out.println("GameActivity::onStop()");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        System.out.println("GameActivity::onDestroy()");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        System.out.println("GameActivity::onPause()");
     }
 }
