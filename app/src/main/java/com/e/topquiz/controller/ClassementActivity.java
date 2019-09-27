3e39ab0a24c4a8954f115daf890a20177b24d328
loots <ludotracks@hotmail.com>
2019-09-27 15:56:13 +0200

    exercice 1

---
+++ app/src/main/java/com/e/topquiz/controller/ClassementActivity.java
@@ -0,0 +1,16 @@
+package com.e.topquiz.controller;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.e.topquiz.R;
+
+public class ClassementActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_classement);
+    }
+}
---
+++ app/src/main/java/com/e/topquiz/model/User.java
@@ -0,0 +1,23 @@
+package com.e.topquiz.model;
+
+public class User {
+    private String mFirstName;
+
+
+    public String getFirstName() {
+        return mFirstName;
+    }
+
+    public void setFirstName(String firstName) {
+        mFirstName = firstName;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "mFirstName='" + mFirstName + '\'' +
+                '}';
+    }
+}
